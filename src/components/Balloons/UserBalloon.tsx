76ae874662f69d54b34e6891218d97db5cd1719b
wesuRage <weslley.nmiranda@gmail.com>
2024-11-09 09:48:57 -0300

    feat(key): added tag's key

--- src/components/Balloons/UserBalloon.tsx
+++ src/components/Balloons/UserBalloon.tsx
@@ -1,14 +1,17 @@
+import { Key } from "react";
+
 interface UserBalloonProps {
-  children: React.ReactNode;
+  key: Key;
+  message: string;
 }
 
-export default function UserBalloon({ children }: UserBalloonProps) {
+export default function UserBalloon({ key, message }: UserBalloonProps) {
   return (
-    <section className="flex justify-between">
+    <section className="flex justify-between" key={key}>
       <div></div>
       <div className="flex">
         <div className="bg-slate-900 p-2 rounded-xl rounded-tr-none">
-          <p>{children}</p>
+          <p>{message}</p>
         </div>
         <div className="w-0 h-0 border-t-[0px] border-t-transparent border-l-[15px] border-l-slate-900 border-b-[15px] border-b-transparent"></div>
       </div>
