0f796c3ab9148144b05994f5581410e320d54bfd
wesuRage <weslley.nmiranda@gmail.com>
2024-11-09 10:23:38 -0300

    feat(balloons): main structure of the page almost done

--- src/app/home/chat/[chat]/page.tsx
+++ src/app/home/chat/[chat]/page.tsx
@@ -1,38 +1,57 @@
+// @ts-nocheck
+
 "use client";
 
 import UserBalloon from "@/components/Balloons/UserBalloon";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import React from "react";
+import { useSession } from "next-auth/react";
+import BotBalloon from "@/components/Balloons/BotBalloon";
 
 export default function Home({ params }: any) {
-  const chat = params.chat; // Extraindo o parâmetro do chat
+  const { chat } = React.use(params);
   const router = useRouter();
-  const [loading, setLoading] = useState(true); // Estado de carregamento
-  const [chatData, setChatData] = useState(null); // Estado para armazenar os dados do chat
+  const [loading, setLoading] = useState(true);
+  const [chatData, setChatData] = useState<Array<any>>([]);
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.replace("/home/practice/chats");
+    },
+  });
+
+  const userId = session?.user.id;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`/api/chat/${chat}`);
-        if (!response.data.data) {
+        if (userId === undefined) return;
+
+        const response = await axios.get(`/api/messages/${userId}/${chat}`);
+
+        if (!response.data || !Array.isArray(response.data.data)) {
           router.push("/not-found");
         } else {
-          setChatData(response.data.data); // Armazena os dados do chat
-          console.log(chatData);
+          setChatData(response.data.data);
         }
       } catch (error) {
         console.error("Error fetching chat data:", error);
         router.push("/not-found");
       } finally {
-        setLoading(false); // Atualiza o estado de carregamento
+        setLoading(false);
       }
     };
 
-    fetchData(); // Chama a função de busca de dados
-  }, [chat, router]);
+    fetchData();
 
-  if (loading) {
+    const intervalId = setInterval(fetchData, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [chat, router, userId]);
+
+  if (loading || !chatData) {
     return (
       <main className="h-full bg-slate-800 rounded-xl p-6 m-6">
         <div>Carregando...</div>
@@ -41,8 +60,26 @@ export default function Home({ params }: any) {
   }
 
   return (
-    <main className="h-full bg-slate-800 rounded-xl p-6 m-6">
-      <UserBalloon>{chat}</UserBalloon>
+    <main className="min-h-[75%] max-h-[75%] overflow-x-hidden overflow-y-scroll bg-slate-800 rounded-xl p-6 m-6">
+      {chatData.map((item: any, index: number) => {
+        if (item.sender === "user") {
+          if (item.content.text) {
+            return <UserBalloon key={index} message={item.content.text} />;
+          }
+        } else {
+          if (item.content.text) {
+            return (
+              <BotBalloon
+                key={index}
+                message={item.content.text}
+                audio={item.content.audioData}
+                translation={item.content.translation}
+              />
+            );
+          }
+        }
+        return null;
+      })}
     </main>
   );
 }
