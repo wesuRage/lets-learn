a2b989df8db184ad275f8fa153fe847d102a3110
wesuRage <weslley.nmiranda@gmail.com>
2024-11-09 09:59:51 -0300

    feat(color): added a red to the title

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,10 +6,11 @@ export default function NotFound() {
   return (
     <main className="flex items-center justify-center h-screen">
       <section className="text-center">
-        <div className="flex items-center text-2xl">
+        <div className="flex items-center text-2xl text-red-500">
           <FaTimes className="relative top-[0.15rem] text-3xl" />
           <p>Page Not Found</p>
         </div>
+        <br />
         <Link
           href="/home/practice/chats"
           className="underline flex items-center justify-center"
